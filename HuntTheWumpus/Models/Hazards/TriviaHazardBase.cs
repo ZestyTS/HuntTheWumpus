7f354b8aad6d559f805c39739daaec96eae913d9
ZestyTS <ZestyTS@gmail.com>
2022-07-08 09:40:26 -0700

    Added TriviaHazardBase

---
+++ HuntTheWumpus/Models/Hazards/TriviaHazardBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuntTheWumpus.Models.Hazards
+{
+    public abstract class TriviaHazardBase : HazardBase
+    {
+        public abstract int TriviaBattleMax { get; }
+        public abstract int TriviaBattleMin { get; }
+        protected TriviaHazardBase(int theme) : base(theme) { }
+
+    }
+}
